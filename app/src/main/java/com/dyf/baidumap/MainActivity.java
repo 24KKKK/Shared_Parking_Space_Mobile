729bd06fb04c325e92cc3f99175de2a231c49b79
24k <2991736073@qq.com>
2017-11-11 14:39:36 +0800

    完成了模拟导航和开始导航
    可以模拟导航和真实导航，导航过程中，有语音播报

--- app/src/main/java/com/dyf/baidumap/MainActivity.java
+++ app/src/main/java/com/dyf/baidumap/MainActivity.java
@@ -1,10 +1,18 @@
 package com.dyf.baidumap;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,7 +41,15 @@ import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.navisdk.adapter.BNCommonSettingParam;
+import com.baidu.navisdk.adapter.BNOuterTTSPlayerCallback;
+import com.baidu.navisdk.adapter.BNRoutePlanNode;
+import com.baidu.navisdk.adapter.BNaviSettingManager;
+import com.baidu.navisdk.adapter.BaiduNaviManager;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -72,6 +88,20 @@ public class MainActivity extends AppCompatActivity
     //覆盖物信息展示
     private RelativeLayout mMarkerLy;
 
+    //导航相关
+    public static List<Activity> activityList = new LinkedList<Activity>();
+    public static final String ROUTE_PLAN_NODE = "routePlanNode";
+    private static final String APP_FOLDER_NAME = "SharedPS";
+    private String mSDCardPath = null;
+    private final static String authBaseArr[] =
+            {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION};
+    private final static String authComArr[] = {Manifest.permission.READ_PHONE_STATE};
+    private final static int authBaseRequestCode = 1;
+    private final static int authComRequestCode = 2;
+
+    private boolean hasInitSuccess = false;
+    private boolean hasRequestComAuth = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -81,6 +111,7 @@ public class MainActivity extends AppCompatActivity
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
         //注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
+        activityList.add(this);
         setContentView(R.layout.activity_main);
 
         this.context = this;
@@ -98,7 +129,7 @@ public class MainActivity extends AppCompatActivity
             public void onMapLongClick(LatLng latLng)
             {
                 //用户长按设置之后，给用户一个提示
-                Toast.makeText(MainActivity.this,"设置目的地成功",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "设置目的地成功", Toast.LENGTH_SHORT).show();
                 //设置终点的位置
                 mDestLocationData = latLng;
                 //给终点设置一下图标
@@ -128,7 +159,12 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-
+                if (mDestLocationData == null)
+                {
+                    Toast.makeText(MainActivity.this,"长按地图设置目标地点",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                routeplanToNavi(false);
             }
         });
 
@@ -138,7 +174,12 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-
+                if (mDestLocationData == null)
+                {
+                    Toast.makeText(MainActivity.this,"长按地图设置目标地点",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                routeplanToNavi(true);
             }
         });
 
@@ -213,6 +254,320 @@ public class MainActivity extends AppCompatActivity
                 return false;
             }
         });
+
+        //导航部分，copy的导航demo，初始化导航相关
+        if (initDirs())
+        {
+            initNavi();
+        }
+    }
+
+    //导航部分，copy的导航demo，初始化导航相关
+    private boolean initDirs()
+    {
+        mSDCardPath = getSdcardDir();
+        if (mSDCardPath == null)
+        {
+            return false;
+        }
+        File f = new File(mSDCardPath, APP_FOLDER_NAME);
+        if (!f.exists())
+        {
+            try
+            {
+                f.mkdir();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    String authinfo = null;
+
+    /**
+     * 内部TTS播报状态回传handler
+     */
+    private Handler ttsHandler = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+            int type = msg.what;
+            switch (type)
+            {
+                case BaiduNaviManager.TTSPlayMsgType.PLAY_START_MSG:
+                {
+                    // showToastMsg("Handler : TTS play start");
+                    break;
+                }
+                case BaiduNaviManager.TTSPlayMsgType.PLAY_END_MSG:
+                {
+                    // showToastMsg("Handler : TTS play end");
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    };
+
+    /**
+     * 内部TTS播报状态回调接口
+     */
+    private BaiduNaviManager.TTSPlayStateListener ttsPlayStateListener = new BaiduNaviManager.TTSPlayStateListener()
+    {
+
+        @Override
+        public void playEnd()
+        {
+            // showToastMsg("TTSPlayStateListener : TTS play end");
+        }
+
+        @Override
+        public void playStart()
+        {
+            // showToastMsg("TTSPlayStateListener : TTS play start");
+        }
+    };
+
+    public void showToastMsg(final String msg)
+    {
+        MainActivity.this.runOnUiThread(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean hasBasePhoneAuth()
+    {
+        // TODO Auto-generated method stub
+
+        PackageManager pm = this.getPackageManager();
+        for (String auth : authBaseArr)
+        {
+            if (pm.checkPermission(auth, this.getPackageName()) != PackageManager.PERMISSION_GRANTED)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasCompletePhoneAuth()
+    {
+        // TODO Auto-generated method stub
+
+        PackageManager pm = this.getPackageManager();
+        for (String auth : authComArr)
+        {
+            if (pm.checkPermission(auth, this.getPackageName()) != PackageManager.PERMISSION_GRANTED)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void initNavi()
+    {
+
+        BNOuterTTSPlayerCallback ttsCallback = null;
+
+        // 申请权限
+        if (android.os.Build.VERSION.SDK_INT >= 23)
+        {
+
+            if (!hasBasePhoneAuth())
+            {
+
+                this.requestPermissions(authBaseArr, authBaseRequestCode);
+                return;
+
+            }
+        }
+
+        BaiduNaviManager.getInstance().init(this, mSDCardPath, APP_FOLDER_NAME, new BaiduNaviManager.NaviInitListener()
+        {
+            @Override
+            public void onAuthResult(int status, String msg)
+            {
+                if (0 == status)
+                {
+                    authinfo = "key校验成功!";
+                } else
+                {
+                    authinfo = "key校验失败, " + msg;
+                }
+                MainActivity.this.runOnUiThread(new Runnable()
+                {
+
+                    @Override
+                    public void run()
+                    {
+                        Toast.makeText(MainActivity.this, authinfo, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            public void initSuccess()
+            {
+                Toast.makeText(MainActivity.this, "百度导航引擎初始化成功", Toast.LENGTH_SHORT).show();
+                hasInitSuccess = true;
+                initSetting();
+            }
+
+            public void initStart()
+            {
+                Toast.makeText(MainActivity.this, "百度导航引擎初始化开始", Toast.LENGTH_SHORT).show();
+            }
+
+            public void initFailed()
+            {
+                Toast.makeText(MainActivity.this, "百度导航引擎初始化失败", Toast.LENGTH_SHORT).show();
+            }
+
+        }, null, ttsHandler, ttsPlayStateListener);
+    }
+
+    private String getSdcardDir()
+    {
+        if (Environment.getExternalStorageState().equalsIgnoreCase(Environment.MEDIA_MOUNTED))
+        {
+            return Environment.getExternalStorageDirectory().toString();
+        }
+        return null;
+    }
+
+    private BNRoutePlanNode.CoordinateType mCoordinateType = null;
+
+    /**
+     * @param mock 设置是否是真实导航，false为模拟，TRUE为真实导航
+     */
+    private void routeplanToNavi(boolean mock)
+    {
+
+        BNRoutePlanNode.CoordinateType coType = BNRoutePlanNode.CoordinateType.BD09LL;
+        mCoordinateType = coType;
+        if (!hasInitSuccess)
+        {
+            Toast.makeText(MainActivity.this, "还未初始化!", Toast.LENGTH_SHORT).show();
+        }
+        // 权限申请
+        if (android.os.Build.VERSION.SDK_INT >= 23)
+        {
+            // 保证导航功能完备
+            if (!hasCompletePhoneAuth())
+            {
+                if (!hasRequestComAuth)
+                {
+                    hasRequestComAuth = true;
+                    this.requestPermissions(authComArr, authComRequestCode);
+                    return;
+                } else
+                {
+                    Toast.makeText(MainActivity.this, "没有完备的权限!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+        BNRoutePlanNode sNode = null;
+        BNRoutePlanNode eNode = null;
+
+        //设置的真实的起点和终点
+        //mLastLocationData
+        //mDestLocationData
+        //sNode = new BNRoutePlanNode(116.30784537597782, 40.057009624099436, "百度大厦", null, coType);
+        //eNode = new BNRoutePlanNode(116.40386525193937, 39.915160800132085, "北京天安门", null, coType);
+        mLastLocationData = new LatLng(mLatitude, mLongtitude);
+        Log.i("我的mLastLocationData",mLastLocationData.toString());
+        Log.i("我的mDestLocationData",mDestLocationData.toString());
+        sNode = new BNRoutePlanNode(mLastLocationData.longitude,mLastLocationData.latitude,"我的地点",null,coType);
+        eNode = new BNRoutePlanNode(mDestLocationData.longitude,mDestLocationData.latitude,"目标地点",null,coType);
+
+        if (sNode != null && eNode != null)
+        {
+            List<BNRoutePlanNode> list = new ArrayList<BNRoutePlanNode>();
+            list.add(sNode);
+            list.add(eNode);
+
+            // 开发者可以使用旧的算路接口，也可以使用新的算路接口,可以接收诱导信息等
+            // BaiduNaviManager.getInstance().launchNavigator(this, list, 1, true, new DemoRoutePlanListener(sNode));
+            BaiduNaviManager.getInstance().launchNavigator(this, list, 1, mock, new DemoRoutePlanListener(sNode),
+                    eventListerner);
+        }
+    }
+
+    BaiduNaviManager.NavEventListener eventListerner = new BaiduNaviManager.NavEventListener()
+    {
+
+        @Override
+        public void onCommonEventCall(int what, int arg1, int arg2, Bundle bundle)
+        {
+            BNEventHandler.getInstance().handleNaviEvent(what, arg1, arg2, bundle);
+        }
+    };
+
+    public class DemoRoutePlanListener implements BaiduNaviManager.RoutePlanListener
+    {
+
+        private BNRoutePlanNode mBNRoutePlanNode = null;
+
+        public DemoRoutePlanListener(BNRoutePlanNode node)
+        {
+            mBNRoutePlanNode = node;
+        }
+
+        @Override
+        public void onJumpToNavigator()
+        {
+            /*
+             * 设置途径点以及resetEndNode会回调该接口
+             */
+
+            for (Activity ac : activityList)
+            {
+                if (ac.getClass().getName().endsWith("BNDemoGuideActivity"))
+                {
+                    return;
+                }
+            }
+            Intent intent = new Intent(MainActivity.this, BNDemoGuideActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putSerializable(ROUTE_PLAN_NODE, (BNRoutePlanNode) mBNRoutePlanNode);
+            intent.putExtras(bundle);
+            startActivity(intent);
+
+        }
+
+        @Override
+        public void onRoutePlanFailed()
+        {
+            // TODO Auto-generated method stub
+            Log.e("TAG","算路失败");
+            Toast.makeText(MainActivity.this, "算路失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initSetting()
+    {
+        // BNaviSettingManager.setDayNightMode(BNaviSettingManager.DayNightMode.DAY_NIGHT_MODE_DAY);
+        BNaviSettingManager
+                .setShowTotalRoadConditionBar(BNaviSettingManager.PreViewRoadCondition.ROAD_CONDITION_BAR_SHOW_ON);
+        BNaviSettingManager.setVoiceMode(BNaviSettingManager.VoiceMode.Veteran);
+        // BNaviSettingManager.setPowerSaveMode(BNaviSettingManager.PowerSaveMode.DISABLE_MODE);
+        BNaviSettingManager.setRealRoadCondition(BNaviSettingManager.RealRoadCondition.NAVI_ITS_ON);
+        BNaviSettingManager.setIsAutoQuitWhenArrived(true);
+        Bundle bundle = new Bundle();
+        // 必须设置APPID，否则会静音
+        bundle.putString(BNCommonSettingParam.TTS_APP_ID, "10277611");
+        BNaviSettingManager.setNaviSdkParam(bundle);
     }
 
     //给终点设置一下图标，设置终点信息
